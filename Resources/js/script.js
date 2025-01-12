e10277a873f095ebd6d65a047c9fd49b68e103c3
Bryant Davis <bryant.davis79@gmail.com>
2025-01-11 19:56:30 -0600

    9

---
+++ Resources/js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("num1").onclick = function write1()
+    {
+        document.getElementById("screen").innerHTML = "1";
+    }
+
+    document.getElementById("clear").onclick = function write1()
+    {
+        document.getElementById("screen").innerHTML = "";
+    }
