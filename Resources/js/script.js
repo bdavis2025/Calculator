31591b58b53074635b852a98b7df023ee92f3684
Bryant Davis <bryant.davis79@gmail.com>
2025-01-11 20:47:09 -0600

    lesson 51

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -1,9 +1,81 @@
+var $screen = $("#screen");
+
 document.getElementById("num1").onclick = function write1()
     {
-        document.getElementById("screen").innerHTML = "1";
+        $screen.append("1");
     }
 
+    document.getElementById("num2").onclick = function write1()
+        {
+            $screen.append("2");
+        }
+
+        document.getElementById("num3").onclick = function write1()
+            {
+                $screen.append("3");
+            }
+
+            document.getElementById("num4").onclick = function write1()
+                {
+                    $screen.append("4");
+                }
+
+                document.getElementById("num5").onclick = function write1()
+                    {
+                        $screen.append("5");
+                    }
+
+                    document.getElementById("num6").onclick = function write1()
+                        {
+                            $screen.append("6");
+                        }
+
+                        document.getElementById("num7").onclick = function write1()
+                            {
+                                $screen.append("7");
+                            }
+
+                            document.getElementById("num8").onclick = function write1()
+                                {
+                                    $screen.append("8");
+                                }
+
+                                document.getElementById("num9").onclick = function write1()
+                                    {
+                                        $screen.append("9");
+                                    }
+
+                                    document.getElementById("num0").onclick = function write1()
+                                        {
+                                            $screen.append("0");
+                                        }
+
     document.getElementById("clear").onclick = function write1()
-    {
-        document.getElementById("screen").innerHTML = "";
-    }
+        {
+            $screen.empty();
+        }
+
+        document.getElementById("plus").onclick = function write1()
+            {
+                $screen.append("+");
+            }
+
+            document.getElementById("minus").onclick = function write1()
+                {
+                    $screen.append("-");
+                }
+
+                document.getElementById("multiply").onclick = function write1()
+                    {
+                        $screen.append("x");
+                    }
+
+                    document.getElementById("divide").onclick = function write1()
+                        {
+                            $screen.append("/");
+                        }
+
+                        document.getElementById("equal").onclick = function write1()
+                            {
+                                $screen.append("=");
+                            }
