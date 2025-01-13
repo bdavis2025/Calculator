2da7d29a89d6b007dc172ffd0ebba32992d4122e
Bryant Davis <bryant.davis79@gmail.com>
2025-01-12 18:00:21 -0600

    lesson 51.0.1

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -5,77 +5,77 @@ document.getElementById("num1").onclick = function write1()
         $screen.append("1");
     }
 
-    document.getElementById("num2").onclick = function write1()
+    document.getElementById("num2").onclick = function write2()
         {
             $screen.append("2");
         }
 
-        document.getElementById("num3").onclick = function write1()
+        document.getElementById("num3").onclick = function write3()
             {
                 $screen.append("3");
             }
 
-            document.getElementById("num4").onclick = function write1()
+            document.getElementById("num4").onclick = function write4()
                 {
                     $screen.append("4");
                 }
 
-                document.getElementById("num5").onclick = function write1()
+                document.getElementById("num5").onclick = function write5()
                     {
                         $screen.append("5");
                     }
 
-                    document.getElementById("num6").onclick = function write1()
+                    document.getElementById("num6").onclick = function write6()
                         {
                             $screen.append("6");
                         }
 
-                        document.getElementById("num7").onclick = function write1()
+                        document.getElementById("num7").onclick = function write7()
                             {
                                 $screen.append("7");
                             }
 
-                            document.getElementById("num8").onclick = function write1()
+                            document.getElementById("num8").onclick = function write8()
                                 {
                                     $screen.append("8");
                                 }
 
-                                document.getElementById("num9").onclick = function write1()
+                                document.getElementById("num9").onclick = function write9()
                                     {
                                         $screen.append("9");
                                     }
 
-                                    document.getElementById("num0").onclick = function write1()
+                                    document.getElementById("num0").onclick = function write0()
                                         {
                                             $screen.append("0");
                                         }
 
-    document.getElementById("clear").onclick = function write1()
+    document.getElementById("clear").onclick = function writeClear()
         {
             $screen.empty();
         }
 
-        document.getElementById("plus").onclick = function write1()
+        document.getElementById("plus").onclick = function writePlus()
             {
                 $screen.append("+");
             }
 
-            document.getElementById("minus").onclick = function write1()
+            document.getElementById("minus").onclick = function writeMinus()
                 {
                     $screen.append("-");
                 }
 
-                document.getElementById("multiply").onclick = function write1()
+                document.getElementById("multiply").onclick = function writeMultiply()
                     {
                         $screen.append("x");
                     }
 
-                    document.getElementById("divide").onclick = function write1()
+                    document.getElementById("divide").onclick = function writeDivide()
                         {
                             $screen.append("/");
                         }
 
-                        document.getElementById("equal").onclick = function write1()
+                        document.getElementById("equal").onclick = function writeEqual()
                             {
                                 $screen.append("=");
                             }
