d51cddc7710b53308366176bab567889bd3c04d6
Bryant Davis <bryant.davis79@gmail.com>
2025-03-26 09:52:22 -0500

    54.0.1

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -1,4 +1,4 @@
-var currentNumber =1;
+var currentNumber = 1;
 var num1;
 var num2;
 
@@ -16,7 +16,7 @@ $number.on('click', function() {
         if (num1 == null) {
           num1 = numberPressed;  
         } else {
-        num1 = num1 + numberPressed
+        num1 = num1 + numberPressed;
         }
     }
         
@@ -30,58 +30,7 @@ $number.on('click', function() {
 });
 
 
-/* document.getElementById("num1").onclick = function write1()
-    {
-        $screen.append("1");
-    }
-
-    document.getElementById("num2").onclick = function write2()
-        {
-            $screen.append("2");
-        }
-
-        document.getElementById("num3").onclick = function write3()
-            {
-                $screen.append("3");
-            }
-
-            document.getElementById("num4").onclick = function write4()
-                {
-                    $screen.append("4");
-                }
-
-                document.getElementById("num5").onclick = function write5()
-                    {
-                        $screen.append("5");
-                    }
-
-                    document.getElementById("num6").onclick = function write6()
-                        {
-                            $screen.append("6");
-                        }
-
-                        document.getElementById("num7").onclick = function write7()
-                            {
-                                $screen.append("7");
-                            }
-
-                            document.getElementById("num8").onclick = function write8()
-                                {
-                                    $screen.append("8");
-                                }
-
-                                document.getElementById("num9").onclick = function write9()
-                                    {
-                                        $screen.append("9");
-                                    }
-
-                                    document.getElementById("num0").onclick = function write0()
-                                        {
-                                            $screen.append("0");
-                                        }
-*/
-
-        $("plus").on('click', function()
+       $("plus").on('click', function()
             {
                 $screen.append("+");
                 op = "+";
