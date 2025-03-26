89f14ad28dadd583030543c38a695b214b88182c
Bryant Davis <bryant.davis79@gmail.com>
2025-03-26 09:48:34 -0500

    54

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -1,5 +1,6 @@
-var num1 = 11;
-var num2 = 11;
+var currentNumber =1;
+var num1;
+var num2;
 
 var $screen = $("#screen");
 var $number = $(".number");
@@ -10,13 +11,24 @@ var $number = $(".number");
 $number.on('click', function() {
     var numberPressed = $(this).html();
     $screen.append(numberPressed);
-    if (num1 == 11) {
-        num1 = numberPressed;
-    } else if (num2 == 11) {
-        num2 = numberPressed;
-    }
 
+    if (currentNumber == 1) {
+        if (num1 == null) {
+          num1 = numberPressed;  
+        } else {
+        num1 = num1 + numberPressed
+        }
+    }
+        
+    if (currentNumber == 2) {
+        if (num2 == null) {
+            num2 = numberPressed;
+        } else {
+            num2 = num2 + numberPressed;
+        }
+    }
 });
+
 
 /* document.getElementById("num1").onclick = function write1()
     {
@@ -68,38 +80,47 @@ $number.on('click', function() {
                                             $screen.append("0");
                                         }
 */
-    document.getElementById("clear").onclick = function writeClear()
-        {
-            $screen.empty();
-        };
 
-        document.getElementById("plus").onclick = function writePlus()
+        $("plus").on('click', function()
             {
                 $screen.append("+");
                 op = "+";
-            }
+                currentNumber++;
+            })
 
-            document.getElementById("minus").onclick = function writeMinus()
+            $("#minus").on('click', function ()
                 {
                     $screen.append("-");
                     op = "-";
-                }
+                    currentNumber++;
+                })
 
-                document.getElementById("multiply").onclick = function writeMultiply()
+                $("#multiply").on('click', function()
                     {
                         $screen.append("x");
                         op = "x";
-                    }
+                        currentNumber++;
+                    })
 
-                    document.getElementById("divide").onclick = function writeDivide()
+                    $("divide").on('click', function()
                         {
                             $screen.append("/");
                             op = "/";
-                        }
+                            currentNumber++;
+                        })
 
-                       document.getElementById("equal").onclick = function writeEqual()
+        $("clear").on('click', function()
+        {
+            $screen.empty();
+            num1 = null;
+            num2 = null;
+            currentNumber = 1;
+        });
+
+                       $("equal").on('click', function()
                             {
                                 $screen.append("=");
+                                console.log(num1, num2);
                                 num1 = parseInt(num1);
                                 num2 = parseInt(num2);
                                 console.log(num1, op, num2);
